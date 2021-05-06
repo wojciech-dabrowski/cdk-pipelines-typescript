1ce39b5a480a30c755dfb8e0e274cf1f86c5d2d7
Wojciech Dąbrowski <wojc.dabrowski@gmail.com>
2021-05-06 10:15:45 +0200

    Correct branch

--- src/pipeline-stack.ts
+++ src/pipeline-stack.ts
@@ -1,8 +1,8 @@
-import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
-import * as cp from "@aws-cdk/aws-codepipeline";
-import * as cpa from "@aws-cdk/aws-codepipeline-actions";
-import * as pipelines from "@aws-cdk/pipelines";
-import { ServerlessApiStage } from "./serverless-api-stage";
+import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import * as cp from '@aws-cdk/aws-codepipeline';
+import * as cpa from '@aws-cdk/aws-codepipeline-actions';
+import * as pipelines from '@aws-cdk/pipelines';
+import { ServerlessApiStage } from './serverless-api-stage';
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -17,25 +17,25 @@ export class PipelineStack extends Stack {
       cloudAssemblyArtifact
     );
 
-    const pipeline = new pipelines.CdkPipeline(this, "Pipeline", {
+    const pipeline = new pipelines.CdkPipeline(this, 'Pipeline', {
       cloudAssemblyArtifact,
       sourceAction,
       synthAction,
     });
 
     // Test
-    const preProdApp = new ServerlessApiStage(this, "Test", {
-      envSufix: "Test",
+    const preProdApp = new ServerlessApiStage(this, 'Test', {
+      envSufix: 'Test',
     });
     const preProdStage = pipeline.addApplicationStage(preProdApp);
     const serviceUrl = pipeline.stackOutput(preProdApp.urlOutput);
 
     preProdStage.addActions(
       new pipelines.ShellScriptAction({
-        actionName: "IntegrationTests",
+        actionName: 'IntegrationTests',
         runOrder: preProdStage.nextSequentialRunOrder(),
         additionalArtifacts: [sourceArtifact],
-        commands: ["npm install", "npm run build", "npm run integration"],
+        commands: ['npm install', 'npm run build', 'npm run integration'],
         useOutputs: {
           SERVICE_URL: serviceUrl,
         },
@@ -43,7 +43,7 @@ export class PipelineStack extends Stack {
     );
 
     // Prod
-    const prodApp = new ServerlessApiStage(this, "Prod", { envSufix: "Prod" });
+    const prodApp = new ServerlessApiStage(this, 'Prod', { envSufix: 'Prod' });
     pipeline.addApplicationStage(prodApp);
   }
 
@@ -54,18 +54,18 @@ export class PipelineStack extends Stack {
     return pipelines.SimpleSynthAction.standardNpmSynth({
       sourceArtifact,
       cloudAssemblyArtifact,
-      buildCommand: "npm run build",
+      buildCommand: 'npm run build',
     });
   }
 
   private createSourceAction(sourceArtifact: cp.Artifact) {
     return new cpa.GitHubSourceAction({
-      actionName: "GitHub",
+      actionName: 'GitHub',
       output: sourceArtifact,
-      oauthToken: SecretValue.secretsManager("github-token"),
-      owner: "wojciech-dabrowski",
-      repo: "cdk-pipelines-demo",
-      branch: "07-cp-prod-stage",
+      oauthToken: SecretValue.secretsManager('github-token'),
+      owner: 'wojciech-dabrowski',
+      repo: 'cdk-pipelines-typescript',
+      branch: 'master',
     });
   }
 }
